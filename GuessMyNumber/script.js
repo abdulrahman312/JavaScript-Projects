c059fc9e095d5cd3622138059385f1dd1d0c03b6
Abdul Rahman Khan <abdulrahman312@yahoo.com>
2021-01-13 16:39:26 +0300

    Update script.js

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,5 +1,6 @@
 let randomNumber;
 let score;
+let guessCount;
 let highScore = [];
 let inputField = document.querySelector('.guess');
 let scoreValue = document.querySelector('.score');
@@ -14,29 +15,42 @@ inputField.addEventListener('keyup', (e) => {
 document.querySelector('.again').addEventListener('click', initializeGame);
 
 function initializeGame() {
-    randomNumber = Math.floor(Math.random() * 20) + 1;
-    score = 0;
-    scoreValue.textContent = 0;
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    score = 10;
+    guessCount = 0;
+    scoreValue.textContent = 10;
     inputField.value = '';
     inputField.focus();
     document.querySelector('.body').style.backgroundColor = "#222";
     document.querySelector('.number').textContent = "?";
+    messageField.textContent = "Start guessing..."
 }
 
 function checkInput() {
-    if (inputField.value >= 1 && inputField.value <= 20) {
-        score++;
-        if (inputField.value == randomNumber) {
-            document.querySelector('.number').textContent = randomNumber;
-            document.querySelector('.body').style.backgroundColor = "green";
-            scoreValue.textContent = score;
-            highScore.push(score);
-            document.querySelector('.highscore').textContent = Math.min(...highScore)
-            messageField.textContent = "Congratulation, You won!!!"
+    if (inputField.value >= 1 && inputField.value <= 100) {
+        if (score > 1) {
+            guessCount++;
+            if (inputField.value == randomNumber) {
+                document.querySelector('.number').textContent = randomNumber;
+                document.querySelector('.body').style.backgroundColor = "green";
+                scoreValue.textContent = score;
+                highScore.push(guessCount);
+                document.querySelector('.highscore').textContent = Math.min(...highScore)
+                messageField.textContent = "Congratulation, You won!!!"
+            } else {
+                score--;
+                if (inputField.value > randomNumber) {
+                    messageField.textContent = "Your guess is too high";
+                } else {
+                    messageField.textContent = "Your guess is too low";
+                }
+                scoreValue.textContent = score;
+                inputField.value = '';
+                inputField.focus();
+            }
         } else {
-            inputField.value = '';
-            inputField.focus();
-            messageField.textContent = "Wrong Guess 😟, Try Again.";
+            messageField.textContent = "Sorry, you lost."
+            scoreValue.textContent = 0;
         }
     } else {
         messageField.textContent = "The INPUT should be a number between 1 and 20.🚫"
