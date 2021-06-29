b34b4db855a0bfee73614ffcc72680db28270262
Abdul Rahman Khan <abdulrahman312@yahoo.com>
2021-06-29 16:00:08 +0300

    Pagination functionality added

--- Forkify/src/js/controller.js
+++ Forkify/src/js/controller.js
@@ -1,19 +1,17 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
-
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const recipeContainer = document.querySelector('.recipe');
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
-
-
-
+// Control single recipe search
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
@@ -32,8 +30,41 @@ const controlRecipes = async function () {
   }
 };
 
+// Control the search results
+const controlSearchResults = async function () {
+  try {
+    // Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    resultsView.renderSpinner();
+
+    // Load search results
+    await model.loadSearchResults(query);
+
+    // Render results
+    resultsView.render(model.getSearchResultsPage());
+
+    // Render initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const controlPagination = function (gotoPage) {
+  // Render results
+  resultsView.render(model.getSearchResultsPage(gotoPage));
+
+  // Render pagination button
+  paginationView.render(model.state.search);
+
+}
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 }
 
 init();
